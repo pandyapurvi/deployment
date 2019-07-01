298037cfa398b918581442f578a054b1c08beac1
Purvi <pandyapurvi134@yahoo.in>
2019-07-01 20:42:48 +1000

    Updates

---
+++ precache-manifest.311db29b52daca1015f3c71e95620f58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2526f51e494e8e53bff63d45c46a52e9",
+    "url": "/deployment/index.html"
+  },
+  {
+    "revision": "25d36e57a65445b101b7",
+    "url": "/deployment/static/css/main.ccc708cf.chunk.css"
+  },
+  {
+    "revision": "c28873cb26fd697cc248",
+    "url": "/deployment/static/js/2.7a1ecdad.chunk.js"
+  },
+  {
+    "revision": "25d36e57a65445b101b7",
+    "url": "/deployment/static/js/main.05f74b7b.chunk.js"
+  },
+  {
+    "revision": "9464ae16e48ae9433d6b",
+    "url": "/deployment/static/js/runtime~main.8aefd960.js"
+  }
+]);
